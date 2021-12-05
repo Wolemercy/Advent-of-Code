516e816a4e805e44a735223ffea6ebdd2aa8ed15
Oluwole Ajewole <52546943+Wolemercy@users.noreply.github.com>
2021-12-05 18:22:36 +0100

    Update day1.py

--- Dec 2021/day1.py
+++ Dec 2021/day1.py
@@ -1,33 +1,34 @@
-inputFile = open('input.txt', 'r')
-data = list(map(int, inputFile.read().splitlines()))
+from typing import List
+
+def get_input(file: str) -> List[int]:
+    data = []
+    with open(file) as f:
+        for line in f.readlines():
+            data.append(int(line.strip()))
+    return data
 
 # Part 1
-def sonar_sweep(measurements: list) -> int:
+def sonar_sweep_one(measurements: List[int]) -> int:
     prev = measurements[0]
-
     count = 0
     for measure in measurements:
         if measure > prev:
             count += 1
         prev = measure
-
-    return count
-
-print(f'Number of increments are {sonar_sweep(data)}')
+    print(f'Number of increments are {count}')
 
 # Part 2
-def sonar_sweep2(measurements: list) -> int:
+def sonar_sweep_two(measurements: List[int]) -> int:
     prev = sum(measurements[0:3])
-
     count = 0
     for i in range(1, len(measurements) - 2):
         curr = prev - measurements[i - 1] + measurements[i + 2]
-
         if curr > prev:
             count += 1
-
         prev = curr
+    print(f'Number of sums larger than previous are {count}')
 
-    return count
-
-print(f'Number of sums larger than previous are {sonar_sweep2(data)}')
+if __name__ == '__main__':
+    puzzle_input = get_input('input.txt')
+    sonar_sweep_one(puzzle_input)
+    sonar_sweep_two(puzzle_input)
